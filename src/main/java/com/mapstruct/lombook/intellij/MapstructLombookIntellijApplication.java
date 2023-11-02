7cb9e15de8680e043d82cda4b391c5b872e886b2
Folau Kaveinga <folaukaveinga@gmail.com>
2023-11-02 10:15:46 -0600

    setup

---
+++ src/main/java/com/mapstruct/lombook/intellij/MapstructLombookIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.mapstruct.lombook.intellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructLombookIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructLombookIntellijApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapstruct/lombook/intellij/MapstructLombookIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapstruct.lombook.intellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructLombookIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
